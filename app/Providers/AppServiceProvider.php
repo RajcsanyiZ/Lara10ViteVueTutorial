4783d18d9f349ab58024d94ca99da41fb672e643
Rajcsányi Zoltán <info@rajcsanyizoltan.hu>
2023-01-06 20:47:24 +0100

    language initialization, Hungarian language added as default

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Inertia\Inertia;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +24,20 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Inertia::share([
+            'locale' => function () {
+                return app()->getLocale();
+            },
+            'language' => function () {
+                if(!file_exists(resource_path('lang/'. app()->getLocale()
+                    .'/'.app()->getLocale() .'.json'))) {
+                    return [];
+                }
+                return json_decode(file_get_contents(
+                        resource_path('lang/'.app()->getLocale() .'/'
+                            .app()->getLocale() .'.json'))
+                    , true);
+            },
+        ]);
     }
 }
